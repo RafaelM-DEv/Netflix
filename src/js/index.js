5ba70eb969e9eca04f788beaa98f1984a25acdc6
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2021-03-24 18:26:38 -0300

    Update estructure archive and webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,3 @@
-import '../css/style.scss'
-import '../css/main.scss'
-import '../css/profiles.scss'
+import '../sass/main.scss'
 
 import '../js/profiles.js'
-
-// import '../assets/avatar_01.png'
-// import '../assets/avatar_02.png'
-// import '../assets/avatar_03.png'
